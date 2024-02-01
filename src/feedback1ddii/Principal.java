f9ebb106c4b67419d5e7aed13499ab03e995583d
AfdezCaat <arantzazufdezcast@gmail.com>
2024-02-01 23:40:26 +0100

    subir formulario

---
+++ src/feedback1ddii/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package feedback1ddii;
+
+/**
+ *
+ * @author Arant
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario wind=new Formulario();
+      
+        wind.setVisible(true);
+
+    }
+    
+}
